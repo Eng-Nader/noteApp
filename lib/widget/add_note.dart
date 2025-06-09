0cde9b1f932b0acf19cd03da2bdbbb3f96c7cfa9
NaderSameer <nadersameer@std.mans.edu.eg>
2025-06-09 06:10:31 +0300

    edit in valdation file

--- lib/widget/add_note.dart
+++ lib/widget/add_note.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/widget/custom_boutton.dart';
-import 'package:note_app/widget/text_fild.dart';
+import 'package:note_app/widget/add_note_form.dart';
 
 class AddNoteBouttom extends StatelessWidget {
   const AddNoteBouttom({super.key});
@@ -10,66 +9,6 @@ class AddNoteBouttom extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: AddFormNote(),
-    );
-  }
-}
-
-class AddFormNote extends StatefulWidget {
-  const AddFormNote({
-    super.key,
-  });
-
-  @override
-  State<AddFormNote> createState() => _AddFormNoteState();
-}
-
-class _AddFormNoteState extends State<AddFormNote> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidatemode = AutovalidateMode.disabled;
-  String? title, subtitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidatemode,
-      child: ListView(
-        children: [
-          const SizedBox(
-            height: 30,
-          ),
-          TextFiled(
-            onSaved: (value) {
-              title = value;
-            },
-            contentpadding: const EdgeInsets.symmetric(vertical: 16),
-            title: '  title',
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          TextFiled(
-            onSaved: (vlaue) {
-              subtitle = vlaue;
-            },
-            contentpadding: const EdgeInsets.symmetric(vertical: 50),
-            title: '  content',
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          CustomBoutton(
-            onPressed: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidatemode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-        ],
-      ),
     );
   }
 }
