4a5bbb0ca2800e390f043bc320b64aa98a04a78b
NaderSameer <nadersameer@std.mans.edu.eg>
2025-06-09 18:02:20 +0300

    create add_noteCubit and state for add note

---
+++ lib/cubits/add_note_state.dart
@@ -0,0 +1,12 @@
+abstract class AddNoteState {}
+
+class AddIntinalNote extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFaliuer extends AddNoteState {
+  final String errorMessage;
+  AddNoteFaliuer(this.errorMessage);
+}
